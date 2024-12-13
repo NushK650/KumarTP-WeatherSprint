014634499cfa83b7315df945fb082b08f51bac59
NushK650 <Nush8653@gmail.com>
2024-12-12 20:57:55 -0800

    a night time commit

--- scripts/app.js
+++ scripts/app.js
@@ -8,10 +8,12 @@ let currentTemp = document.getElementById("currentTemp");
 let currentIcon = document.getElementById("currentIcon");
 let currentCondition = document.getElementById("currentCondition");
 let feelsLike = document.getElementById("feelsLike");
-let currentHiLow = document.getElementById("currentHiLow");
+let todayHiLow = document.getElementById("todayHiLow");
 
 let searchBar = document.getElementById("searchBar");
 
+let todayIcon = document.getElementById("todayIcon")
+let todayTemp = document.getElementById("todayTemp")
 
 let day1Title = document.getElementById("day1Title");
 let day2Title = document.getElementById("day2Title");
@@ -106,15 +108,12 @@ async function asycnGetData(city) {
   currentTemp.innerText =` ${Math.round(data.main.temp)}°`;
   currentCondition.innerText = data.weather[0].main;
   feelsLike.innerText = `Feels Like: ${Math.round(data.main.feels_like)}`;
-  currentHiLow.innerText = `hi: ${Math.round(
-    data.main.temp_max
-  )} / low: ${Math.round(data.main.temp_min)}`;
-
-  if(data.weather[0].main == "Haze"){
-    currentIcon.innerHTML
-
-  }
-}
+  todayHiLow.innerText = `hi: ${Math.round(data.main.temp_max)} / low: ${Math.round(data.main.temp_min)}`;
+currentIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].description}">`;
+todayIcon.innerHTML= currentIcon.innerHTML;
+  todayTemp.innerText = currentTemp.innerText;
+  
+};
 
 async function asycnGetForecast(city) {
   const promise = await fetch(
@@ -125,19 +124,19 @@ async function asycnGetForecast(city) {
   for (let i = 0; i < 8; i++) {
     sumDay1 += data.list[i].main.temp;
     tempsOfDay1.push(data.list[i].main.temp);
-  }
+  };
   const averageDay1 = sumDay1 / 8;
   day1Temp.innerText = Math.round(averageDay1);
   // Jacob and w3 schools helped me with this I wanted to try something new
-  day1HighLow.innerText = `${Math.round(
-    Math.max(...tempsOfDay1)
-  )}°F / ${Math.round(Math.min(...tempsOfDay1))}°F`;
-currentIcon.innerHTML
-day0
-day1Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[4].icon}@2x.png" alt="${data.weather[4].description}">`;
-day2Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[12].icon}@2x.png" alt="${data.weather[12].description}">`;
-day3Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[20].icon}@2x.png" alt="${data.weather[20].description}">`;
-day4Icon.innerHTML =`<img src="http://openweathermap.org/img/wn/${data.weather[28].icon}@2x.png" alt="${data.weather[28].description}">`;
+  day1HighLow.innerText = `${Math.round(Math.max(...tempsOfDay1))}°F / ${Math.round(Math.min(...tempsOfDay1))}°F`;
+
+
+day1Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[4].weather[0].icon}@2x.png" alt="${data.list[4].weather[0].description}">`;
+day2Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[12].weather[0].icon}@2x.png" alt="${data.list[12].weather[0].description}">`;
+day3Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[20].weather[0].icon}@2x.png" alt="${data.list[20].weather[0].description}">`;
+day4Icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[28].weather[0].icon}@2x.png" alt="${data.list[28].weather[0].description}">`;
+
+
   for (let i = 9; i < 16; i++) {
     sumDay2 += data.list[i].main.temp;
     tempsOfDay2.push(data.list[i].main.temp);
@@ -145,9 +144,7 @@ day4Icon.innerHTML =`<img src="http://openweathermap.org/img/wn/${data.weather[2
   const averageDay2 = sumDay2 / 8;
   day2Temp.innerText = Math.round(averageDay2);
   // Jacob and w3 schools helped me with this I wanted to try something new
-  day2HighLow.innerText = `${Math.round(
-    Math.max(...tempsOfDay2)
-  )}°F / ${Math.round(Math.min(...tempsOfDay2))}°F`;
+  day2HighLow.innerText = `${Math.round(Math.max(...tempsOfDay2))}°F / ${Math.round(Math.min(...tempsOfDay2))}°F`;
 
 
 
@@ -159,9 +156,7 @@ day4Icon.innerHTML =`<img src="http://openweathermap.org/img/wn/${data.weather[2
   const averageDay3 = sumDay3 / 8;
   day3Temp.innerText = Math.round(averageDay3);
   // Jacob and w3 schools helped me with this I wanted to try something new
-  day3HighLow.innerText = `${Math.round(
-    Math.max(...tempsOfDay3)
-  )}°F / ${Math.round(Math.min(...tempsOfDay3))}°F`;
+  day3HighLow.innerText = `${Math.round(Math.max(...tempsOfDay3))}°F / ${Math.round(Math.min(...tempsOfDay3))}°F`;
 
 
 
@@ -175,9 +170,7 @@ day4Icon.innerHTML =`<img src="http://openweathermap.org/img/wn/${data.weather[2
   const averageDay4 = sumDay4 / 8;
   day4Temp.innerText = Math.round(averageDay4);
   // Jacob and w3 schools helped me with this I wanted to try something new
-  day4HighLow.innerText = `${Math.round(
-    Math.max(...tempsOfDay4)
-  )}°F / ${Math.round(Math.min(...tempsOfDay4))}°F`;
+  day4HighLow.innerText = `${Math.round(Math.max(...tempsOfDay4))}°F / ${Math.round(Math.min(...tempsOfDay4))}°F`;
 
 
 
